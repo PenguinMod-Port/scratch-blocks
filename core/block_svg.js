bd07750cfa128a64c04ae36bbab689bca5113f6c
jwklong <72522395+jwklong@users.noreply.github.com>
2026-02-17 12:09:11 +0000

    fix

--- core/block_svg.js
+++ core/block_svg.js
@@ -37,7 +37,6 @@ goog.require('Blockly.scratchBlocksUtils');
 goog.require('Blockly.Tooltip');
 goog.require('Blockly.Touch');
 goog.require('Blockly.utils');
-goog.require('Blockly.FieldExpandable');
 
 goog.require('goog.Timer');
 goog.require('goog.asserts');
