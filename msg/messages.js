461f6da6ed29d9906f8c341753f19cbb92c310d8
SharkPool <139097378+SharkPool-SP@users.noreply.github.com>
2026-09-06 12:20:42 -0700

    Pin warning messages

--- msg/messages.js
+++ msg/messages.js
@@ -565,6 +565,8 @@ Blockly.Msg.PM_MOVE_BY_CATEGORY = 'Organize by Category';
 Blockly.Msg.PM_PIN = 'Pin';
 Blockly.Msg.PM_UNPIN = 'Unpin';
 Blockly.Msg.PM_UNPIN_ALL = 'Unpin All';
+Blockly.Msg.NO_PINS = 'No Pinned Blocks!';
+Blockly.Msh.MISSING_PIN = 'Some Pins could not load!';
 
 // Color
 Blockly.Msg.COLOUR_HUE_LABEL = 'Color';
